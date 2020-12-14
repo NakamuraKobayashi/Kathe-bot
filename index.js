009aea9920dd2a37a49d5e764d89abf9a0d4ff20
Anush Kumar <53186299+anush-kumar@users.noreply.github.com>
2020-12-14 11:26:23 +0530

    Fourth Commit

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const path = require('path')
 const fs = require('fs')
 const Discord = require('discord.js')
-const { Mongoose } = require('mongoose')
 const client = new Discord.Client()
-const config = require('./config.json')
-const mongo = require('./mongo.js')
-const { dir } = require('console')
 
+const mongo = require('./mongo.js')
+const { Mongoose } = require('mongoose')
+
+const config = require('./config.json')
 
 //MongoDB Connection 
 const connecToMongoDB = async () => {
@@ -21,27 +21,28 @@ const connecToMongoDB = async () => {
 
 connecToMongoDB()
 
-//Connected Log 
-client.on('ready', () => {
-  console.log('Katheryne Has Been Back To Work')
+//Client Check Log
+client.on('ready', async () => {
+  console.log('The client is ready!')
 
   const baseFile = 'command-base.js'
   const commandBase = require(`./commands/${baseFile}`)
 
-  const readCommands = dir => {
+  const readCommands = (dir) => {
     const files = fs.readdirSync(path.join(__dirname, dir))
     for (const file of files) {
-      const stat = fs.lstatSync(path.join(__dirname,dir, file))
+      const stat = fs.lstatSync(path.join(__dirname, dir, file))
       if (stat.isDirectory()) {
-        readCommands(path.join(dir, file)) 
-      } else if (file != baseFile) {
+        readCommands(path.join(dir, file))
+      } else if (file !== baseFile) {
         const option = require(path.join(__dirname, dir, file))
-        console.log(file, option)
+        commandBase(client, option)
       }
     }
   }
+
   readCommands('commands')
 })
 
 client.login(process.env.DJS_TOKEN)
-//client.login(config.token)
+//client.login("Nzg2NDIwMTkyMDQwNTgzMjQ4.X9GI4A.GFdcuFoGthRYcVcm3po5mdOqjzo")
