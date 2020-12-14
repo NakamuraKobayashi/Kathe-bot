caa2251043764574378b2bcfe743993ca83985ee
Anush Kumar <53186299+anush-kumar@users.noreply.github.com>
2020-12-14 09:16:35 +0530

    Third Submit

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+const path = require('path')
+const fs = require('fs')
 const Discord = require('discord.js')
 const { Mongoose } = require('mongoose')
 const client = new Discord.Client()
+const config = require('./config.json')
 const mongo = require('./mongo.js')
-//const config = require('./config.json')
+const { dir } = require('console')
+
 
 //MongoDB Connection 
 const connecToMongoDB = async () => {
@@ -20,6 +24,23 @@ connecToMongoDB()
 //Connected Log 
 client.on('ready', () => {
   console.log('Katheryne Has Been Back To Work')
+
+  const baseFile = 'command-base.js'
+  const commandBase = require(`./commands/${baseFile}`)
+
+  const readCommands = dir => {
+    const files = fs.readdirSync(path.join(__dirname, dir))
+    for (const file of files) {
+      const stat = fs.lstatSync(path.join(__dirname,dir, file))
+      if (stat.isDirectory()) {
+        readCommands(path.join(dir, file)) 
+      } else if (file != baseFile) {
+        const option = require(path.join(__dirname, dir, file))
+        console.log(file, option)
+      }
+    }
+  }
+  readCommands('commands')
 })
 
 client.login(process.env.DJS_TOKEN)
