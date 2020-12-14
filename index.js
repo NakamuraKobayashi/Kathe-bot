d2924fea723ea921c07b6707823e1d57df8cb2b2
Anush Kumar <53186299+anush-kumar@users.noreply.github.com>
2020-12-14 13:08:06 +0530

    Fifth Commit

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-const path = require('path')
-const fs = require('fs')
 const Discord = require('discord.js')
 const client = new Discord.Client()
+const loadCommands = require('./commands/load-commands')
 
 const mongo = require('./mongo.js')
 const { Mongoose } = require('mongoose')
@@ -25,23 +24,7 @@ connecToMongoDB()
 client.on('ready', async () => {
   console.log('The client is ready!')
 
-  const baseFile = 'command-base.js'
-  const commandBase = require(`./commands/${baseFile}`)
-
-  const readCommands = (dir) => {
-    const files = fs.readdirSync(path.join(__dirname, dir))
-    for (const file of files) {
-      const stat = fs.lstatSync(path.join(__dirname, dir, file))
-      if (stat.isDirectory()) {
-        readCommands(path.join(dir, file))
-      } else if (file !== baseFile) {
-        const option = require(path.join(__dirname, dir, file))
-        commandBase(client, option)
-      }
-    }
-  }
-
-  readCommands('commands')
+  loadCommands(client)
 })
 
 client.login(process.env.DJS_TOKEN)
