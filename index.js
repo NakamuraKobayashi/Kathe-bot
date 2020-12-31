6850c922055e549eeff611433d3970b9e9fe8de8
Anush Kumar <53186299+anush-kumar@users.noreply.github.com>
2020-12-31 09:13:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,10 +9,6 @@ const { Mongoose } = require('mongoose')
 const config = require('./config.json')
 const welcome = require('./welcome')
 
-client.db = require("quick.db");
-client.request = new (require("rss-parser"))();
-client.config = require("./config.js");
-
 //Client Check Log
 client.on('ready', async () => {
   console.log('Kathe Chellam is Ready to work Checking with...')
@@ -22,30 +18,7 @@ client.on('ready', async () => {
   loadCommands(client)
   roleClaim(client)
   welcome(client)
-  handleUploads()
-});
-
-function handleUploads() {
-  if (client.db.fetch(`postedVideos`) === null) client.db.set(`postedVideos`, []);
-  setInterval(() => {
-      client.request.parseURL(`https://www.youtube.com/feeds/videos.xml?channel_id=${client.config.channel_id}`)
-      .then(data => {
-          if (client.db.fetch(`postedVideos`).includes(data.items[0].link)) return;
-          else {
-              client.db.set(`videoData`, data.items[0]);
-              client.db.push("postedVideos", data.items[0].link);
-              let parsed = client.db.fetch(`videoData`);
-              let channel = client.channels.cache.get(client.config.channel);
-              if (!channel) return;
-              let message = client.config.messageTemplate
-                  .replace(/{author}/g, parsed.author)
-                  .replace(/{title}/g, Discord.Util.escapeMarkdown(parsed.title))
-                  .replace(/{url}/g, parsed.link);
-              channel.send(message);
-          }
-      });
-  }, client.config.watchInterval);
-}
+})
 
 client.login(process.env.DJS_TOKEN)
 //client.login("Nzg2NDIwMTkyMDQwNTgzMjQ4.X9GI4A.GFdcuFoGthRYcVcm3po5mdOqjzo")
