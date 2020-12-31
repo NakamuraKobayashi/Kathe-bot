065509662eab4cf32d21be6b0e56d6a29fdb6511
Anush Kumar <53186299+anush-kumar@users.noreply.github.com>
2020-12-31 08:37:53 +0530

    Update 3

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    channel: "794037983174328321",
+    messageTemplate: "Hello @everyone, **{author}** just now uploaded a video **{title}**!\n{url}",
+    channel_id: "UCv3PZzlgZpfg7Jp41mdtQew",
+    watchInterval: 30000
+}
