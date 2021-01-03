126444c438e31d4359d39bdb051482cd4d2b2ca7
Anush Kumar <53186299+anush-kumar@users.noreply.github.com>
2021-01-03 23:02:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const welcome = require('./welcome')
 client.on('ready', async () => {
   console.log('Kathe Chellam is Ready to work Checking with...')
 
-  client.user.setActivity('Genshin Impact | 52 Travalers');
+  client.user.setActivity('Genshin Impact | 54 Travalers');
 
   loadCommands(client)
   roleClaim(client)
