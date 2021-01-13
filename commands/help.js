013bbc49b9703c070057dccaf736832d4e156d66
Anush Kumar <53186299+anush-kumar@users.noreply.github.com>
2021-01-13 14:47:59 +0530

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -13,7 +13,7 @@ module.exports = {
         .addFields(
             { name: 'Fun Commands - 4', value: '`hello`,`vaada`,`skip`,`pity`' },
             { name: 'List All Players UID:', value: '`list, l`' },
-            { name: 'Bot Commands: ', value: '`help, h`' },
+            { name: 'Bot Commands: ', value: '`help`' },
         )
         .setFooter('Use p! Before Any Command | Example: p!hello');
         message.channel.send(help)
